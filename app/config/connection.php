5861b0d9e554ab26525b4f0b443e41b6f24cd6c3
DarkHotTea <105120676+DarkHotTea@users.noreply.github.com>
2023-11-14 14:55:05 +0300

    Update connection.php

--- app/config/connection.php
+++ app/config/connection.php
@@ -1,6 +1,6 @@
 <?php 
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'TestTask');
+define('DB_USER', 'freetest1_test123');
+define('DB_PASS', 'rJ6kC5dO7l');
+define('DB_NAME', 'freetest1_test123');
